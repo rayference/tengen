fad604b4f35f402c3890d4711f93ef074f1ae882
Yvan Nollet <yvan.nollet@rayference.eu>
2022-06-25 00:08:01 +0200

    Fix pre-commit session

--- tests/test_util.py
+++ tests/test_util.py
@@ -13,7 +13,7 @@ def test_to_data_set() -> None:
     """Returns a data set."""
     ds = to_data_set(
         w=np.linspace(1, 2) * ureg.nm,
-        ssi=np.random.random(50) * ureg.watt / ureg.m ** 2 / ureg.nm,
+        ssi=np.random.random(50) * ureg.watt / ureg.m**2 / ureg.nm,
         url="imaginary url",
     )
     assert isinstance(ds, xr.Dataset)
@@ -23,7 +23,7 @@ def test_to_data_set_attrs() -> None:
     """Returns a data set."""
     ds = to_data_set(
         w=np.linspace(1, 2) * ureg.nm,
-        ssi=np.random.random(50) * ureg.watt / ureg.m ** 2 / ureg.nm,
+        ssi=np.random.random(50) * ureg.watt / ureg.m**2 / ureg.nm,
         url="imaginary url",
         attrs=dict(
             title="test",
@@ -40,7 +40,7 @@ def test_to_data_set_t_not_none() -> None:
     """Returns a data set when t is not None."""
     ds = to_data_set(
         w=np.linspace(1, 2) * ureg.nm,
-        ssi=np.random.random((31, 50)) * ureg.watt / ureg.m ** 2 / ureg.nm,
+        ssi=np.random.random((31, 50)) * ureg.watt / ureg.m**2 / ureg.nm,
         url="imaginary url",
         t=pd.date_range(
             start=datetime.date(2021, 1, 1), end=datetime.date(2021, 1, 31)
